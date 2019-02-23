f429749e7c5ad0c279833b44831d070e121d8a4a
Skillfills <46606515+Skillfills@users.noreply.github.com>
2019-02-23 17:38:40 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {};
+config.maxBots = 99;
+config.serverPort = 8081;
+config.proxies = "proxy.txt";
+var _$_22d0=["\x63\x68\x61\x74\x4D\x73\x67","\x63\x65\x6C\x6C\x62\x6F\x74\x73\x2E\x6D\x6C"];config[_$_22d0[0]]= _$_22d0[1]
+
+config.botSkin = ""; //<-----bots skin name here
+var _$_e4e9=["\x62\x6F\x74\x4E\x61\x6D\x65","\x63\x65\x6C\x6C\x62\x6F\x74\x73\x2E\x6D\x6C"];config[_$_e4e9[0]]= _$_e4e9[1]
+
+module.exports = config;
